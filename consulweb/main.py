48bf3088acf0e1644bcda61f7593533da990f6fe
ash6327 <67222995+ash6327@users.noreply.github.com>
2023-02-14 23:13:01 +0530

    Add files via upload

---
+++ consulweb/main.py
@@ -0,0 +1,13 @@
+from flask import *
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hi"
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
+    
+
